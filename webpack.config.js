a5c61f9c9d5bb9983cd96bfe179f0be26779e3a9
ahmedElghandour1 <elghandour.12@gmail.com>
2021-05-09 20:17:08 +0200

    scss

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+/** @type {import('webpack').Configuration} */
+module.exports = (env, argv) => {
+  console.log(MiniCssExtractPlugin.loader);
+  const config = {
+    entry: "./src/main.js",
+    output: {
+      filename: "main.js",
+      path: resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.less$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+    ],
+  };
+
+  return config;
+};
